14ca1baba99d5f5d9014c176bc7c3f09b204dc8b
Baxtbek11 <umarov.baxtbek.mr@gmail.com>
2025-11-12 16:26:43 +0500

    formlar va ozim yartmoqchi bolgan websayt

---
+++ form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        button:hover{
+            color: black;
+            border: 2px solid ;
+            border-radius: 5px ;
+            background-color: black;
+        }
+    </style>
+</head>
+<body>
+    <form action="form_data.php" method="POST">
+        <label for="">Ism</label>
+        <input type="text" name="ism" id="">
+        <label for="">Familiya</label>
+        <input type="text" name="familiya" id="">
+        <button type="Submit">Saqlash</button>
+    </form>
+</body>
+</html>
